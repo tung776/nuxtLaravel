5bff2e833f7e3dd8715bf771ff4b2eb25d54cea0
tung776 <thanhtung776@gmail.com>
2019-01-04 10:11:02 +0700

    nuxt auth supported

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,65 +1,106 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios',
-    // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
-  ],
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      
+     ** Headers of the page
+     */
+    head: {
+        title: pkg.name,
+        meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: pkg.description
+            }
+        ],
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico'
+        }]
+    },
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: {
+        color: '#fff'
+    },
+
+    /*
+     ** Global CSS
+     */
+    css: [],
+
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://github.com/nuxt-community/axios-module#usage
+        '@nuxtjs/axios',
+        // Doc: https://bootstrap-vue.js.org/docs/
+        'bootstrap-vue/nuxt',
+        '@nuxtjs/auth'
+    ],
+
+    /*
+     ** Auth module configuration
+     ** https://auth.nuxtjs.org/reference/schemes/local
+     */
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: {
+                        url: '/api/login',
+                        method: 'post',
+                        propertyName: 'meta.token'
+                    },
+                    logout: {
+                        url: '/api/logout',
+                        method: 'post'
+                    },
+                    user: {
+                        url: '/api/user',
+                        method: 'get',
+                        propertyName: 'data'
+                    }
+                },
+                // tokenRequired: true,
+                // tokenType: 'bearer',
+            }
+        }
     }
-  }
+    /*
+     ** Axios module configuration
+     */
+    axios: {
+        // See https://github.com/nuxt-community/axios-module#options
+        baseURL: "http://localhost:8000/api"
+    },
+
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {
+
+        }
+    }
 }
