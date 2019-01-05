6c8ea4c1cfa42d851e3c980dedd24f904a919a65
tung776 <thanhtung776@gmail.com>
2019-01-05 10:47:19 +0700

    topic post model

--- server/app/Post.php
+++ server/app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected fillable = ['body'];
+    
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function topic() {
+        return $this->belongsTo(Topic::class);
+    }
 }
--- server/app/Topic.php
+++ server/app/Topic.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
+    protected fillable = ['title'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
 }
